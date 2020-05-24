f90785fbd7a49236023b3a65185a142450080bf5
Arthur Barré <arthur.barre@easy.movie>
2020-05-24 15:09:46 +0200

    clean app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ export default function App() {
     <div className="App">
       <Chat />
       <Chat message="of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and m" />
-      <Chat
-        icon
-        hour="12h30"
-        username={text("UserName", "shatice")}
-        message={text("Message", "Mais ou et donc or ni car")}
-      />
-      <Chat
-        hour="4:30"
-        message={text("Message", "This is a message with icon")}
-      />
+      <Chat icon hour="12h30" username="shatice" message="hello all" />
+      <Chat hour="4:30" message="Hello" mention="helene" icon />
       );
     </div>
   );
--- src/ui/atoms/Highlight.js
+++ src/ui/atoms/Highlight.js
@@ -3,31 +3,23 @@ import Colors from "../particles/Colors";
 import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 
-// const isDisplayed = {
-//   true: "inline",
-//   false: "none",
-// }
-
 const HighlightMessageWrapper = styled.span` 
-  // display ${props => (props.display === isDisplayed.true ? isDisplayed.true : isDisplayed.false)};
-  // color: ${Colors.text_secondary};
-  // margin: 0 5px 0 0;
+  color: ${Colors.text_secondary};
+  margin: 0 5px 0 0;
 `;
 
-const HighlightMessage = ({ display }) => {
+const HighlightMessage = () => {
   return (
-  <HighlightMessageWrapper display={display}>
+  <HighlightMessageWrapper>
     10:32
   </HighlightMessageWrapper>
   );
 };
 
 HighlightMessage.propTypes = {
-  // display: PropTypes.bool,
 };
 
 HighlightMessage.defaultProps = {
-  // display: isDisplayed.true, 
 };
 
 export default HighlightMessage;
